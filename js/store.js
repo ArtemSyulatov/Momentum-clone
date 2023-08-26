180816563ff33cb6a0dd386201fcf0c8351f5c5e
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2023-08-26 23:57:35 +0500

    renameSounds

--- js/store.js
+++ js/store.js
@@ -1,24 +1,24 @@
 const music = [
   {
-    title: "Aqua Caelestis",
+    title: "AquaCaelestis",
     src: "../assets/sounds/Aqua Caelestis.mp3",
     duration: "00:39",
     id:0
   },
   {
-    title: "Ennio Morricone",
+    title: "EnnioMorricone",
     src: "../assets/sounds/Ennio Morricone.mp3",
     duration: "1:37",
     id:1
   },
   {
-    title: "River Flows In You",
+    title: "RiverFlowsInYou",
     src: "../assets/sounds/River Flows In You.mp3",
     duration: "1:37",
     id:2
   },
   {
-    title: "Summer Wind",
+    title: "SummerWind",
     src: "../assets/sounds/Summer Wind.mp3",
     duration: "1:50",
     id:3
