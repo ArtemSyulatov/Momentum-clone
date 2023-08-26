0fe0377a1f20455fb1cbe47300ced0fb50845629
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2023-08-27 00:27:31 +0500

    fix paths

--- js/store.js
+++ js/store.js
@@ -1,25 +1,25 @@
 const music = [
   {
     title: "Aqua Caelestis",
-    src: "../assets/sounds/AquaCaelestis.mp3",
+    src: "./assets/sounds/AquaCaelestis.mp3",
     duration: "00:39",
     id:0
   },
   {
     title: "Ennio Morricone",
-    src: "../assets/sounds/EnnioMorricone.mp3",
+    src: "./assets/sounds/EnnioMorricone.mp3",
     duration: "1:37",
     id:1
   },
   {
     title: "River Flows In You",
-    src: "../assets/sounds/RiverFlowsInYou.mp3",
+    src: "./assets/sounds/RiverFlowsInYou.mp3",
     duration: "1:37",
     id:2
   },
   {
     title: "Summer Wind",
-    src: "../assets/sounds/SummerWind.mp3",
+    src: "./assets/sounds/SummerWind.mp3",
     duration: "1:50",
     id:3
   },
