a831920fc41b5395a4cfedb353ef7a7c32482e31
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2023-08-26 23:18:27 +0500

    player added

--- js/store.js
+++ js/store.js
@@ -3,21 +3,25 @@ const music = [
     title: "Aqua Caelestis",
     src: "../assets/sounds/Aqua Caelestis.mp3",
     duration: "00:39",
+    id:0
   },
   {
     title: "Ennio Morricone",
     src: "../assets/sounds/Ennio Morricone.mp3",
     duration: "1:37",
+    id:1
   },
   {
     title: "River Flows In You",
     src: "../assets/sounds/River Flows In You.mp3",
     duration: "1:37",
+    id:2
   },
   {
     title: "Summer Wind",
     src: "../assets/sounds/Summer Wind.mp3",
     duration: "1:50",
+    id:3
   },
 ];
 
