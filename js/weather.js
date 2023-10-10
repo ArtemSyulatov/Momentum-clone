c6f796edeb316d51bf36068fd7eb8aab9116ba7c
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2023-10-10 22:52:18 +0500

    add imgs to local folder

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const weatherDescription = document.querySelector(".weather-description");
 const city = document.querySelector(".city");
 
 async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=1ec3467aa1a557cc376ad77551ecc835&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&cnt=10&lang=ru&appid=1ec3467aa1a557cc376ad77551ecc835&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
   weatherIcon.className = "weather-icon owf";
